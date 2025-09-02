4887f3a7c4cc2c0530c8d937d5df60d81088a7ef
icebox <qqqdyz@gmail.com>
2025-09-03 05:44:02 +0800

    this is a good place to stop for now

--- website/app.py
+++ website/app.py
@@ -2,24 +2,25 @@ from flask import Flask, render_template
 from flask_wtf import FlaskForm
 from wtforms import StringField, FileField
 from wtforms.validators import DataRequired
+from os import urandom
 import sqlite3, functions
 
 from data_parsing import read_eml
+from .forms import *
 
 
 flask_app = Flask(__name__)
+flask_app.secret_key = urandom(64)
+
 
 @flask_app.route('/')
 def home():
-    return render_template("base.html")
+    return render_template("home.html")
 
-@flask_app.route('/emailuploads')
-def emailuploads():
+@flask_app.route('/upload_email', methods=['GET', 'POST'])
+def upload_email():
+    form = EmailUploadForm()
+    if form.validate_on_submit():
+        return 'success!'
+    return render_template('upload_email.html', form=form)
     
-    return True
-
-print(functions.has_url('hello https:welcome.com'))
-
-
-
-
