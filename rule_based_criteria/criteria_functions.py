cd60e7e192e31b898ff5cf81144dc9b3da4ccda3
teck1341 <teck1341@gmail.com>
2025-09-24 17:32:59 +0800

    virustotal_url

--- rule_based_criteria/criteria_functions.py
+++ rule_based_criteria/criteria_functions.py
@@ -5,9 +5,10 @@ from urllib.parse import urlparse
 import re, ipaddress, virustotal_python, os
 from pprint import pprint
 from hashlib import sha256
+from base64 import urlsafe_b64encode
 
 #VirusTotal API Key
-virusTotal_API_key = "7d7b6d2eb25c613fecc1b3a5bb5ccd1cdfeecc6312f390a6237c3f66036a4de3"
+virusTotal_API_key = ""
 
 def sender_domain_checker(email: ProcessedEmail):
    
@@ -15,10 +16,10 @@ def sender_domain_checker(email: ProcessedEmail):
     #print(f"Clean email (without any extra words): {clean_email}"), not sure if still needed
 
     #blacklist
-    blacklist = open((r'C:\Users\teck1\Documents\GitHub\inf1002\email_validation\blacklist.txt')).read().split(" ")
+    blacklist = open((r'\blacklist.txt')).read().split(" ")
 
     #whitelist
-    whitelist = open((r'C:\Users\teck1\Documents\GitHub\inf1002\email_validation\whitelistf1.txt')).read().split(" ")
+    whitelist = open((r'\whitelistf1.txt')).read().split(" ")
 
     domain = email.sender.split("@")[1]
     
@@ -109,19 +110,32 @@ def urL_ip_checker(text):
 
 def virusTotal(email: ProcessedEmail):
     dict = {}
-    for attachment in email.attachments:
-        file_bytes  = attachment.getvalue()
-        file_name = getattr(attachment, "name", "myfile.bin")
-        file_hash = sha256(file_bytes).hexdigest()
-        files = {"file": (file_name, file_bytes)}
-        with virustotal_python.Virustotal(virusTotal_API_key) as vtotal:
-            #will upload into the virustotal, if the file have never been uploaded before
-            vtotal.request("files", files=files, method="POST")
-            #if the file been uploaded before, will instant get the report, else will be error
-            resp = vtotal.request(f"files/{file_hash}").data 
-            attribute_totalvotes = resp["attributes"]["total_votes"]
-            #more info about the attributes: https://docs.virustotal.com/reference/private-files-api#:~:text=You%20can%20find%20information%20about%20the%20file%27s%20attributes%20in%20here.%20In%20this%20case%20the%20SHA256%20of%20the%20file%20is%206b4bb405d3deea7a63e3ed02dd62a59c69b9458c15a901f3607429325b228ae8%3A 
-            dict[file_name] = attribute_totalvotes
+    url_obj = url_detection(email.message)
+    if email.attachments:
+        for attachment in email.attachments:
+            file_bytes  = attachment.getvalue()
+            file_name = getattr(attachment, "name", "myfile.bin")
+            file_hash = sha256(file_bytes).hexdigest()
+            files = {"file": (file_name, file_bytes)}
+            with virustotal_python.Virustotal(virusTotal_API_key) as vtotal:
+                #will upload into the virustotal, if the file have never been uploaded before
+                vtotal.request("files", files=files, method="POST")
+                #if the file been uploaded before, will instant get the report, else will be error
+                resp = vtotal.request(f"files/{file_hash}").data 
+                dict[file_name]= resp["attributes"]["total_votes"]
+                #more info about the attributes: https://docs.virustotal.com/reference/private-files-api#:~:text=You%20can%20find%20information%20about%20the%20file%27s%20attributes%20in%20here.%20In%20this%20case%20the%20SHA256%20of%20the%20file%20is%206b4bb405d3deea7a63e3ed02dd62a59c69b9458c15a901f3607429325b228ae8%3A 
+    elif url_obj:
+       for url_link in url_obj:
+           with virustotal_python.Virustotal(virusTotal_API_key) as vtotal:
+            try:
+                resp = vtotal.request("urls", data={"url": url_link}, method="POST")
+                # Safe encode URL in base64 format
+                # https://developers.virustotal.com/reference/url
+                url_id = urlsafe_b64encode(url_link.encode()).decode().strip("=")
+                report = vtotal.request(f"urls/{url_id}").data
+                dict[url_link] = report["attributes"]["total_votes"]
+            except virustotal_python.VirustotalError as err:
+                print(f"Failed to send URL: {url_link} for analysis and get the report: {err}")
     return dict
         
 def Url_Checker_Overall(email: ProcessedEmail):
