97767f2c984e4596f100aeeada975fcdc8504921
icebox <qqqdyz@gmail.com>
2025-09-26 19:05:21 +0800

    trivialities

--- rule_based_criteria/main.py
+++ rule_based_criteria/main.py
@@ -1,6 +1,6 @@
 from data_parsing import ProcessedEmail
 from . import criteria_functions
-
+from dataset_generation import COMPILED_DATASETS
 class Report:
     def __init__(self, email: ProcessedEmail):
         self.email = email
@@ -18,14 +18,20 @@ class Report:
             }
         ]
 
-    def classify(self):
+    def classify(self) -> dict:
         for criterium in self.criteria:
             self.scores[criterium['name']] = criterium['function'](self.email)
         return self.scores
 
 
+# ok this is where shit actually happens
 
-
-
+def get_average_scores_of_dataset():
+    emails = COMPILED_DATASETS['emails']
+    for email in emails:
+        print(email)
+        email = type('ProcessedEmail', (object,), email) # python magic
+        score_classification = Report(email).classify()
+        print(score_classification)
 
 
